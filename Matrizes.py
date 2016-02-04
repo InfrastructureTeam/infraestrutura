4a2942372a05750b67e653b2c0a091742c43b866
Bruno Victor <brunovf1@gmail.com>
2016-02-04 18:19:03 -0300

    limpando o codigo

--- Matrizes.py
+++ Matrizes.py
@@ -5,19 +5,12 @@ import matplotlib.pyplot as plt
 
 class Calculo():
     def __init__(self, matriz, dist):
-        # self.matrizNula = [[0, 0, 0, 0, 0, 0], [2, 27.06, 25.07, 29.44, 23.48, 27.85],
-        #                    [4, 20.69, 19.10, 35.81, 21.88, 19.50], [6, 17.24, 15.38, 15.12, 16.18, 16.31],
-        #                    [8, 16.91, 18.18, 17.47, 18.00, 20.09], [16, 6.86, 4.97, 5.47, 4.77, 5.99],
-        #                    [32, 1.15, 1.42, 0.97, 0.92, 2.05]]
         self.matrizNula = matriz
         self.dist = dist
-        self.linha = len(self.dist)
-        self.coluna = len(self.matrizNula[0])
         self.matrizResistividade = self.transforma_resistividade(self.matrizNula, self.dist)
         self.vetorMedia = [self.media(array) for array in self.matrizResistividade]
         self.matrizCorrigida = self.medidas_corretas(self.matrizResistividade, self.vetorMedia)
         self.vetorMediaCorrecao = [self.media(array) for array in self.matrizCorrigida]
-        print(self.matrizCorrigida)
         
 
     def transforma_resistividade(self, matriz, dist): #Como o terrometro mostra a resistencia entre os eletrodos este metodo transformara as resistencias em resistividades
@@ -44,16 +37,8 @@ class Calculo():
         '''
         return [[r for r in matriz[i] if(((r - m) / m)<=.5) ] for i, m in enumerate(media)]
 
-    def media_correcao(self):
-        for i in range(self.linha):
-            for j in range(self.coluna):
-                self.vetorMediaCorrecao[i] +=  self.matrizCorrigida[i][j]
-            self.vetorMediaCorrecao[i] /= (self.coluna)
-
 	
 def main():
-	# col = int(input("Em quantos locais diferentes as medidas foram realizadas"))
-	# lin = int(input("Qual a quantidade de distancias diferentes foram utilizadas para os locais"))
 	parser = argparse.ArgumentParser()
 	parser.add_argument('file', help='csv file with data')
 	args = parser.parse_args()
@@ -65,15 +50,10 @@ def main():
 		dist = [item[0] for item in data] # distancias
 		values = [ item[1:] for item in data] # valores
 		matriz = Calculo(values, dist)
-		# matriz.transforma_resistividade()
-		# matriz.media()
-		# matriz.medidas_corretas()
-		# matriz.media_correcao()
-		plt.plot([2, 4, 6, 8, 16, 32], matriz.vetorMediaCorrecao)
+		plt.plot(matriz.dist, matriz.vetorMediaCorrecao)
 		plt.xlabel('Distancia (m)')
 		plt.ylabel('Resistividade (ohm*m)')
 		plt.show()
-
 		print(matriz.matrizNula)
 		print(matriz.matrizCorrigida)
 		print(matriz.vetorMedia)
